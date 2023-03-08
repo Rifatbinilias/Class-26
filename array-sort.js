2942cf61e91cfe14dff5a4db5b4c83ce558aac3c
Rifatbinilias <122632297+Rifatbinilias@users.noreply.github.com>
2023-03-09 01:08:34 +0600

    Add files via upload

---
+++ array-sort.js
@@ -0,0 +1,9 @@
+//এইটা হলো এলা মেলো থাকলেও সিরিয়াল অনুযায়ী দেখাবে। 
+/* const number = [1, 7, 8, 4, 5, 2, 3, 6, 9];
+const numberShortList = number.sort();
+console.log(numberShortList); */
+
+//ঠিক আগের টা মতো ছোট থেকে  বড়
+/* const names = ['Florin', 'Liam', 'Jai', 'Ivan'];
+names.sort();
+console.log(names); */
